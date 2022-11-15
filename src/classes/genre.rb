4afe4fe6d3e89c2e1d680954d5065753ab0de671
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-11-15 11:21:40 +0100

    Fix offences

--- src/classes/genre.rb
+++ src/classes/genre.rb
@@ -1,14 +1,13 @@
-# rubocop:disable Lint/EmptyClass
 class Genre
-    attr_reader :name, :item
-    def initialize(name, item)
-        @id = rand(1..100)
-        @name = name
-        @item = []
-    end
+  attr_reader :name, :item
 
-    def add_item(item)
-        @item << item
-    end
+  def initialize(name, _item)
+    @id = rand(1..100)
+    @name = name
+    @item = []
+  end
+
+  def add_item(item)
+    @item << item
+  end
 end
-# rubocop:enable Lint/EmptyClass
--- src/classes/musicalbum.rb
+++ src/classes/musicalbum.rb
@@ -1,15 +1,14 @@
-# rubocop:disable Lint/EmptyClass
 require_relative 'item'
 
 class MuicAlbum < Item
-    attr_accessor :on_spotify
-    def initialize
-        @on_spotify=on_spotify
-    end
+  attr_accessor :on_spotify
 
-    def can_be_archived?
-        on_spotify == true
-    end
+  def initialize
+    super()
+    @on_spotify = on_spotify
+  end
 
+  def can_be_archived?
+    on_spotify == true
+  end
 end
-# rubocop:enable Lint/EmptyClass
