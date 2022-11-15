913b1e9e2c100576ac4a315bb9b93b7f42477be1
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-11-15 11:01:22 +0100

    Create MusicAlbum and Genre class and initialize them

--- src/classes/genre.rb
+++ src/classes/genre.rb
@@ -1,4 +1,10 @@
 # rubocop:disable Lint/EmptyClass
 class Genre
+    attr_reader :name, :item
+    def initialize(name, item)
+        @id = rand(1..100)
+        @name = name
+        @item = []
+    end
 end
 # rubocop:enable Lint/EmptyClass
--- src/classes/musicalbum.rb
+++ src/classes/musicalbum.rb
@@ -1,5 +1,11 @@
 # rubocop:disable Lint/EmptyClass
+require_relative 'item'
 
-class MuicAlbum
+class MuicAlbum < Item
+    attr_accessor :on_spotify
+    def initialize
+        @on_spotify=on_spotify
+    end
+
 end
 # rubocop:enable Lint/EmptyClass
