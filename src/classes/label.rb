8fff4eb6048c1b0390a9802f3bc489e13ad12cdc
Aleazar yilma <yilma4060@gmail.com>
2022-11-16 17:39:46 +0300

    add label properties and method

--- src/classes/label.rb
+++ src/classes/label.rb
@@ -1,5 +1,17 @@
 # rubocop:disable Lint/EmptyClass
 
 class Label
+    attr_accessor :items, :name, :id
+
+    def initialize(name, )
+      @id = Random.rand(1..1000)
+      @name = name
+      @items = []
+    end
+  
+    def add_item(item)
+      @items << item
+      item.label = self
+    end
 end
 # rubocop:enable Lint/EmptyClass
