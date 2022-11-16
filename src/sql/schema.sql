414d35f72cc97716b2e7baf69e67b71b86115c2a
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-11-17 00:51:35 +0100

    Update schema to include tables for genre and music_albums

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -49,3 +49,23 @@ CREATE TABLE label(
   name VARCHAR(30)
 );
 
+CREATE TABLE genres (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(150) NOT NULL
+)
+
+CREATE TABLE music_albums(
+  id BIGSERIAL PRIMARY KEY,
+  type_of_item text
+  label_id INTEGER,
+  genre_id INTEGER,
+  author_id INTEGER,
+  source_id INTEGER,
+  publish_date DATE,
+  archived BOOLEAN,
+  ON_SPOTIFY BOOLEAN,
+  FOREIGN KEY (label_id) REFERENCES labels(id),
+  FOREIGN KEY (genre_id) REFERENCES genres(id),
+  FOREIGN KEY (author_id) REFERENCES authors(id),
+  FOREIGN KEY (source_id) REFERENCES sources(id)
+);
